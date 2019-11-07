216818360cd24a4bdf90e73657b59b0589483a8b
Kevin Allen <kevin.allen91@gmail.com>
2019-11-07 10:48:14 -0800

    added zoos and animal-zoos tables

--- data/migrations/20191107102458_bootstrapdb.js
+++ data/migrations/20191107102458_bootstrapdb.js
@@ -15,8 +15,32 @@ exports.up = function(knex) {
             .references('id')
             .inTable('species')
             .onDelete('RESTRICT') // about deleting the record from the primary key table. Could be 'RESTRICT', 'DO NOTHING', 'SET NULL' etc
-            .onUpdate() // about changing the value of the primary key
+            .onUpdate('CASCADE') // about changing the value of the primary key
 
+    })
+    .createTable('zoos', tbl => {
+        tbl.increments()
+        tbl.string('name', 255).notNullable()
+        tbl.string('address', 255).notNullable()
+    })
+    .createTable('animal-zoos', tbl => {
+        tbl.increments()
+        tbl
+            .integer('zoo-id')
+            .unsigned()
+            .references('id')
+            .inTable('zoos')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl
+            .integer('animal-id')
+            .unsigned()
+            .references('id')
+            .inTable('animals')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.date('to').notNullable()
+        tbl.date('from')
     })
 
   
